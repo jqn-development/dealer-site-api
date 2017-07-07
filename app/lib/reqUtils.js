74cf35fb0d9ece515089941ba46e24d6c9841f5d
Jay Reardon <jayreardon@gmail.com>
2017-07-07 15:55:48 -0600

    Added request utilities library

---
+++ app/lib/reqUtils.js
@@ -0,0 +1,44 @@
+// app/lib/reqUtils.js
+
+const
+  _                   = require('./lodashExt')
+;
+
+// TODO: Refactor all controllers to use this class instead of reqCheck
+// This class exists to move repeated functionality out of controllers to reduce
+// repeated code
+class ReqUtils {
+  constructor(req) {
+    this.req = req;
+  }
+
+  hasResponse(req) {
+    req = req || this.req;
+    return ((req) && ((req.hasData || req.hasError)));
+  }
+
+  hasRequiredParams(paramList) {
+    // Check Required parameters
+    paramList = paramList || {};
+    let reqParams = [];
+    for (let key in paramList) {
+      if (_.isUnset(paramList[key])) reqParams.push(key);
+    }
+    return reqParams;
+  }
+
+  setError(code, req) {
+    req = req || this.req;
+    req.hasError = true;
+    req.respCode = code;
+  }
+
+  setData(data, req) {
+    req = req || this.req;
+    req.hasData = true;
+    req.data = data;
+  }
+
+}
+
+module.exports = ReqUtils;
