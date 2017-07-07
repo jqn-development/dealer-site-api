9dcac6e9aaf22e26191ccf96444e370fd569fecc
Jay Reardon <jayreardon@gmail.com>
2017-07-07 12:19:44 -0600

    Added logger optimizations dependent on the deployment environment

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -14,32 +14,39 @@ const
 class Logger {
   constructor(config) {
     this.logDir = config.logging.logDir || './logs';
+    let transports = [
+      new (winston.transports.File) ({
+        filename: this.logDir + '/info.log',
+        name: 'info-log',
+        level: 'info',
+        formatter: this.formatter
+      }),
+      new (winston.transports.File) ({
+        filename: this.logDir + '/error.log',
+        name: 'error-log',
+        level: 'error',
+        formatter: this.formatter
+      })
+      // TODO: Add CloudWatch Loggging
+      // new (winstonCloudWatch) ({
+      //   logGroupName: '',
+      //   logStreamName: ''
+      // })
+    ];
+
+    // Optimization -- Add console logging if not in production
+    if (process.env.NODE_ENV !== 'production'){
+      transports.push(
+        new (winston.transports.Console) ({
+          formatter: this.formatter
+        })
+      );
+    }
+
     this.options = {
       exitOnError: false,
       formatter: this.formatter,
-      transports: [
-        new (winston.transports.Console) ({
-          formatter: this.formatter
-        }),
-        /* Add CloudWatch Loggging
-        new (winstonCloudWatch) ({
-          logGroupName: '',
-          logStreamName: ''
-        }),
-        */
-        new (winston.transports.File) ({
-          filename: this.logDir + '/info.log',
-          name: 'info-log',
-          level: 'info',
-          formatter: this.formatter
-        }),
-        new (winston.transports.File) ({
-          filename: this.logDir + '/error.log',
-          name: 'error-log',
-          level: 'error',
-          formatter: this.formatter
-        })
-      ]
+      transports: transports
     };
     // Merge options from config into this object
     this.option = _.assign(this.options, config.logging.options);
@@ -48,6 +55,10 @@ class Logger {
 
   formatter(options) {
     return `${new Date().toISOString()} [${options.level.toUpperCase()}]: ${options.message}`;
+  }
+
+  logError(endpoint, errInfo) {
+    this.log.error(`${endpoint} --- ${JSON.stringify(errInfo)}`);
   }
 }
 
