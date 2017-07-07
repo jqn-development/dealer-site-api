d1145802a657db8cde73963fc4e55bdcba1cf0e0
Jay Reardon <jayreardon@gmail.com>
2017-07-07 15:52:58 -0600

    Stub admin routes

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -14,6 +14,19 @@ module.exports = function(app, controllers, log) {
   // admin Create API Key
   app.post('/admin/createAPIKey', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
 
-  // admin Create siteConfig
-  app.post('/admin/createSite', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+  // admin Create Site
+  //app.post('/admin/createSite', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+  //app.post('/admin/createSite/:dealerID', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+
+  // admin Delete Site
+  //app.post('/admin/deleteSite', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+  //app.post('/admin/deleteSite/:dealerID', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+
+  // admin flush cache
+  //app.post('/admin/flushCache', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+
+  // admin Permissions assignment
+  //app.post('/admin/grantSitePermissions', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+  //app.post('/admin/revokeSitePermissions', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
+
 }
