819c55dc599cad8466add0161b96e33937cb0ff0
Jay Reardon <jayreardon@gmail.com>
2017-07-17 15:01:00 -0600

    Updated admin endpoint routes and controller logic

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,6 +2,7 @@
 
 const
   _                   = require('../lib/lodashExt')
+  , uuidv4            = require('uuid/v4')
   , uuidAPIKey        = require('uuid-apikey')
   , secretKey         = require('secret-key')
   , ReqUtils          = require('../lib/reqUtils')
@@ -20,10 +21,15 @@ class AdminController {
   constructor(dbconn, models, cache, log) {
     this.log = log;
     this.cache = cache;
-    this.model = new models.ACL(dbconn, log).model;
+    this.acl = new models.ACL(dbconn, log);
+    this.model = this.acl.model;
+    this.modelACLSite = this.acl.modelACLSite;
     this.modelSite = new models.Site(dbconn,log).model;
   }
 
+  // =======================================================
+  //
+  // =======================================================
   createAPIKey(req, res, next) {
     let reqUtils = new ReqUtils(req);
 
@@ -50,7 +56,7 @@ class AdminController {
         } else {
           acl.id = acl.apiKey;
           acl.apiKey = uuidAPIKey.toAPIKey(acl.id);
-          reqUtils.setData(_.pick(acl, ['id', 'apiKey', 'secretKey', 'lastUpdated', 'created']));
+          reqUtils.setData(_.pick(acl.dataValues, ['id', 'apiKey', 'secretKey', 'lastUpdated', 'created']));
           next();
         }
       })
@@ -69,15 +75,219 @@ class AdminController {
 
     reqUtils.handleRequest({
       params: {
-        dealerID: { type: 'int', source: ['params', 'body', 'headers', 'query'], required: true }
+        dealerID: { type: 'int', source: ['params', 'body', 'headers', 'query'], required: true },
+        baseDomainURL: { type: 'fqdn', source: ['body', 'headers', 'query'], required: true },
+        title: { type: 'string', source: ['body', 'headers', 'query'] },
+        keywords: { type: 'string', source: ['body', 'headers', 'query'] },
+        description: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelRetailNew: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelRetailUsed: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelInternetNew: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelInternetUsed: { type: 'string', source: ['body', 'headers', 'query'] },
+        bookLabel: { type: 'string', source: ['body', 'headers', 'query'] },
+        supressStockPhotos: { type: 'boolean', source: ['body', 'headers', 'query'], default: false },
       },
       security: { super: true, server: true }
     },
     (req, res, next) => {
-      // TODO: create site logic
+      let siteID = uuidv4();
+      let obj = req.locals;
+      obj.siteID = siteID;
+      this.modelSite.create(obj)
+      .then((site) => {
+        if (!site) {
+          reqUtils.setError(500002);
+          next(`The new site could not be created.`);
+        } else {
+          reqUtils.setData(site);
+          next();
+        }
+      })
+      .catch((err) => {
+        reqUtils.setError(500001);
+        next(err);
+      });
     }, next, res);
   }
 
+  // =======================================================
+  //
+  // =======================================================
+  updateSite(req, res, next) {
+    let reqUtils = new ReqUtils(req);
+
+    reqUtils.handleRequest({
+      params: {
+        siteID: { type: 'uuid', source: ['params', 'body', 'headers', 'query'], required: true },
+        dealerID: { type: 'int', source: ['body', 'headers', 'query'], },         // Do not allow the dealerID to be updated on client side
+        baseDomainURL: { type: 'fqdn', source: ['body', 'headers', 'query'] },    // Do not allow the baseDomainURL to be updated on client side
+        title: { type: 'string', source: ['body', 'headers', 'query'] },
+        keywords: { type: 'string', source: ['body', 'headers', 'query'] },
+        description: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelRetailNew: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelRetailUsed: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelInternetNew: { type: 'string', source: ['body', 'headers', 'query'] },
+        priceLabelInternetUsed: { type: 'string', source: ['body', 'headers', 'query'] },
+        bookLabel: { type: 'string', source: ['body', 'headers', 'query'] },
+        supressStockPhotos: { type: 'boolean', source: ['body', 'headers', 'query'], default: false },
+      },
+      security: { super: true, server: true }
+    },
+    (req, res, next) => {
+      let obj = req.locals;
+      this.modelSite.update(obj, { where: { siteID: req.local.siteID } } )
+      .then((site) => {
+        if (!site) {
+          reqUtils.setError(500002);
+          next(`The new site could not be created.`);
+        } else {
+          reqUtils.setData(site);
+          next();
+        }
+      })
+      .catch((err) => {
+        reqUtils.setError(500001);
+        next(err);
+      });
+    }, next, res);
+  }
+
+  // =======================================================
+  //
+  // =======================================================
+  deleteSite(req, res, next) {
+    let reqUtils = new ReqUtils(req);
+
+    reqUtils.handleRequest({
+      params: {
+        siteID: { type: 'uuid', source: ['params', 'body', 'headers', 'query'], required: true }
+      },
+      security: { super: true, server: true }
+    },
+    (req, res, next) => {
+      let obj = {};
+      obj.isDeleted = true;
+      this.modelSite.update(obj, { where: { siteID: req.locals.siteID} })
+      .then((site) => {
+        if (!site) {
+          reqUtils.setError(500002);
+          next(`The site ${req.locals.siteID} could not be deleted.`);
+        } else {
+          reqUtils.setData({ message: `Site (${req.locals.siteID}) was deleted.` });
+          next();
+        }
+      })
+      .catch((err) => {
+        reqUtils.setError(500001);
+        next(err);
+      });
+    }, next, res);
+  }
+
+  // =======================================================
+  //
+  // =======================================================
+  grantSitePermissions(req, res, next) {
+    let reqUtils = new ReqUtils(req);
+
+    reqUtils.handleRequest({
+      params: {
+        siteID: { type: 'uuid', source: ['body', 'headers', 'query'], required: true },
+        targetAPIKey: { type: 'apikey', source: ['body', 'headers', 'query'] }
+      },
+      security: { super: true, server: true }
+    },
+    (req, res, next) => {
+      let key = req.acl.apiKey;
+      if (_.hasValue(req.locals.targetAPIKey)) key = uuidAPIKey.toUUID(req.locals.targetAPIKey);
+      this.model.find({ where: { apiKey: key }})
+      .then((acl) => {
+        if (!acl) {
+          // The API Key could not be found
+          reqUtils.setError(401001);
+          next(`The API Key provided is invalid.`);
+        } else {
+          let obj = { aclID: acl.aclID, siteID: req.locals.siteID };
+          this.modelACLSite.create(obj)
+          .then((aclSite) => {
+            reqUtils.setData({ message: 'Success' });
+            next();
+          })
+          .catch((err) => {
+            reqUtils.setError(500001);
+            next(err);
+          });
+        }
+      })
+      .catch((err) => {
+        reqUtils.setError(500001);
+        next(err);
+      });
+    }, next, res);
+  }
+
+  // =======================================================
+  //
+  // =======================================================
+  revokeSitePermissions(req, res, next) {
+    let reqUtils = new ReqUtils(req);
+
+    reqUtils.handleRequest({
+      params: {
+        siteID: { type: 'uuid', source: ['body', 'headers', 'query'], required: true },
+        targetAPIKey: { type: 'apikey', source: ['body', 'headers', 'query'] }
+      },
+      security: { super: true, server: true }
+    },
+    (req, res, next) => {
+      let key = req.acl.apiKey;
+      if (_.hasValue(req.locals.targetAPIKey)) key = uuidAPIKey.toUUID(req.locals.targetAPIKey);
+      this.model.find({ where: { apiKey: key }})
+      .then((acl) => {
+        if (!acl) {
+          // The API Key could not be found
+          reqUtils.setError(401001);
+          next(`The API Key provided is invalid.`);
+        } else {
+          let options = { where: { aclID: acl.aclID, siteID: req.locals.siteID } };
+          this.modelACLSite.destroy( options )
+          .then((aclSite) => {
+            reqUtils.setData({ message: 'Success' });
+            next();
+          })
+          .catch((err) => {
+            reqUtils.setError(500001);
+            next(err);
+          });
+        }
+      })
+      .catch((err) => {
+        reqUtils.setError(500001);
+        next(err);
+      });
+    }, next, res);
+  }
+
+
+  // =======================================================
+  //
+  // =======================================================
+  getPermissions(req, res, next) {
+    let reqUtils = new ReqUtils(req);
+
+    reqUtils.handleRequest({
+      params: {},
+      security: { super: true, server: true }
+    },
+    (req, res, next) => {
+      reqUtils.setData(req.acl);
+      next();
+    }, next, res);
+  }
+
+  // =======================================================
+  //
+  // =======================================================
   flushCache(req, res, next) {
     let reqUtils = new ReqUtils(req);
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -10,18 +10,27 @@ module.exports = function(app, controllers, log) {
   app.post('/admin/createAPIKey', controllers.AdminController.createAPIKey.bind(controllers.AdminController));
 
   // admin Create Site
-  app.post('/admin/createSite', controllers.StubController.action.bind(controllers.StubController));
-  app.post('/admin/createSite/:dealerID', controllers.StubController.action.bind(controllers.StubController));
+  app.post('/admin/createSite', controllers.AdminController.createSite.bind(controllers.AdminController));
+  app.post('/admin/createSite/:dealerID', controllers.AdminController.createSite.bind(controllers.AdminController));
+
+  // admin Update Site
+  app.post('/admin/updateSite', controllers.AdminController.updateSite.bind(controllers.AdminController));
+  app.post('/admin/updateSite/:siteID', controllers.AdminController.updateSite.bind(controllers.AdminController));
 
   // admin Delete Site
-  app.post('/admin/deleteSite', controllers.StubController.action.bind(controllers.StubController));
-  app.post('/admin/deleteSite/:dealerID', controllers.StubController.action.bind(controllers.StubController));
+  app.post('/admin/deleteSite', controllers.AdminController.deleteSite.bind(controllers.AdminController));
+  app.post('/admin/deleteSite/:siteID', controllers.AdminController.deleteSite.bind(controllers.AdminController));
+
+  // admin Get Permission
+  app.post('/admin/getPermissions', controllers.AdminController.getPermissions.bind(controllers.AdminController));
 
   // admin flush cache
   app.post('/admin/flushCache', controllers.AdminController.flushCache.bind(controllers.AdminController));
 
   // admin Permissions assignment
-  app.post('/admin/grantSitePermissions', controllers.StubController.action.bind(controllers.StubController));
-  app.post('/admin/revokeSitePermissions', controllers.StubController.action.bind(controllers.StubController));
+  app.post('/admin/grantSitePermissions', controllers.AdminController.grantSitePermissions.bind(controllers.AdminController));
+  app.post('/admin/revokeSitePermissions', controllers.AdminController.revokeSitePermissions.bind(controllers.AdminController));
 
+  // Catch any other /admin requests that do not match
+  app.get('/admin/*', controllers.StubController.action.bind(controllers.StubController));
 }
