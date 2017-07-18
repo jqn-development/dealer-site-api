a74bdb869b8d3b6b0d0f4a43f948cc2394cbf1ef
Jay Reardon <jayreardon@gmail.com>
2017-07-18 17:46:10 -0600

    Added pre-cleanup to unit tests and updated test names

--- test/restAPI.js
+++ test/restAPI.js
@@ -42,6 +42,9 @@ describe('REST Endpoint Tests', () => {
     server.init();
     svr = server.server;
 
+    // Pre-Cleanup Test Data
+    results = await server.dbconn.conn.query('DELETE FROM vehicle WHERE vehicleID < 1', { type: server.dbconn.conn.QueryTypes.DELETE });
+    results = await server.dbconn.conn.query('DELETE FROM dealer WHERE dealerID < 1', { type: server.dbconn.conn.QueryTypes.DELETE });
     // Add dealer and vehicle test data to database so can test updating
     results = await server.dbconn.conn.query('INSERT INTO dealer (dealerID, dealerName, address, city, regionCode, postalCode, phone) VALUES (-1, \'Test Dealer\', \'1234 Winding Way\', \'Broomfield\', \'CO\', \'81234\', \'8005551234\')', { type: server.dbconn.conn.QueryTypes.INSERT });
     results = await server.dbconn.conn.query(
@@ -85,12 +88,12 @@ describe('REST Endpoint Tests', () => {
   // Root Controller Tests
   // =============================================================
   describe('Root Controller', () => {
-    it('check root (/site/) w/o authorization', (done) => {
+    it('checking root (/site/) w/o authorization', (done) => {
       request(svr)
         .get('/site/')
         .expect(401, done);
     });
-    it('check root (/site/) w/ authorization', (done) => {
+    it('checking root (/site/) w/ authorization', (done) => {
       request(svr)
         .get('/site/')
         .query({ apiKey: badTestCreds.apiKey })
@@ -111,7 +114,7 @@ describe('REST Endpoint Tests', () => {
       }
     });
 
-    it('check createAPIKey POST (/site/admin/createAPIKey', (done) => {
+    it('checking createAPIKey POST (/site/admin/createAPIKey', (done) => {
       request(svr)
         .post('/site/admin/createAPIKey')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret })
@@ -127,7 +130,7 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check createSite POST (/site/admin/createSite)', (done) => {
+    it('checking createSite POST (/site/admin/createSite)', (done) => {
       request(svr)
         .post('/site/admin/createSite')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, targetAPIKey: testCreds.apiKey, dealerID: testDealer, baseDomainURL: 'example.com' })
@@ -140,19 +143,19 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check updateSite POST (/site/admin/updateSite)', (done) => {
+    it('checking updateSite POST (/site/admin/updateSite)', (done) => {
       request(svr)
         .post('/site/admin/updateSite')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, siteID: targetSiteID, title: 'Test Site' })
         .expect(200, done);
     });
-    it('check grantSitePermissions POST (/site/admin/grantSitePermissions)', (done) => {
+    it('checking grantSitePermissions POST (/site/admin/grantSitePermissions)', (done) => {
       request(svr)
         .post('/site/admin/grantSitePermissions')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, targetAPIKey: testCreds.apiKey, siteID: targetSiteID })
         .expect(200, done);
     });
-    it('check getPermissions POST (/site/admin/getPermissions)', (done) => {
+    it('checking getPermissions POST (/site/admin/getPermissions)', (done) => {
       request(svr)
         .post('/site/admin/getPermissions')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret })
@@ -164,13 +167,13 @@ describe('REST Endpoint Tests', () => {
   // Dealer Controller Tests
   // =============================================================
   describe('Dealer Controller', () => {
-    it('check dealer GET (/site/dealer) w/o dealerID', (done) => {
+    it('checking dealer GET (/site/dealer) w/o dealerID', (done) => {
       request(svr)
         .get('/site/dealer')
         .query({ apiKey: testCreds.apiKey })
         .expect(400, done);
     });
-    it('check dealer GET (/site/dealer) w/ dealerID', (done) => {
+    it('checking dealer GET (/site/dealer) w/ dealerID', (done) => {
       request(svr)
         .get('/site/dealer')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer })
@@ -182,13 +185,13 @@ describe('REST Endpoint Tests', () => {
   // Vehicle Controller Tests
   // =============================================================
   describe('Vehicle Controller', () => {
-    it('check dealer GET (/site/vehilce/all) w/o dealerID', (done) => {
+    it('checking dealer GET (/site/vehilce/all) w/o dealerID', (done) => {
       request(svr)
         .get('/site/vehicle/all')
         .query({ apiKey: testCreds.apiKey })
         .expect(400, done);
     });
-    it('check dealer GET (/site/vehilce/all) w/ dealerID', (done) => {
+    it('checking dealer GET (/site/vehilce/all) w/ dealerID', (done) => {
       request(svr)
         .get('/site/vehicle/all')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer })
@@ -200,7 +203,7 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check dealer GET (/site/vehilce/all) w/ dealerID and limit', (done) => {
+    it('checking dealer GET (/site/vehilce/all) w/ dealerID and limit', (done) => {
       request(svr)
         .get('/site/vehicle/all')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer, limit: 10 })
@@ -212,19 +215,19 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check dealer GET (/site/vehicle) w/ vehicleID w/o dealerID', (done) => {
+    it('checking dealer GET (/site/vehicle) w/ vehicleID w/o dealerID', (done) => {
       request(svr)
         .get('/site/vehicle')
         .query({ apiKey: testCreds.apiKey, vehicleID: testVehicle })
         .expect(400, done);
     });
-    it('check dealer GET (/site/vehicle) w/ vehicleID w/ wrong dealerID', (done) => {
+    it('checking dealer GET (/site/vehicle) w/ vehicleID w/ wrong dealerID', (done) => {
       request(svr)
         .get('/site/vehicle')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer + 1, vehicleID: testVehicle })
         .expect(403, done);
     });
-    it('check dealer GET (/site/vehicle) w/ vehicleID', (done) => {
+    it('checking dealer GET (/site/vehicle) w/ vehicleID', (done) => {
       request(svr)
         .get('/site/vehicle')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer, vehicleID: testVehicle })
@@ -236,13 +239,13 @@ describe('REST Endpoint Tests', () => {
   // Site (Config) Controller Tests
   // =============================================================
   describe('Site Controller', () => {
-    it('check config GET (/site/config) w/ mismatched APIKey/SiteID', (done) => {
+    it('checking config GET (/site/config) w/ mismatched APIKey/SiteID', (done) => {
       request(svr)
         .get('/site/config')
         .query({ apiKey: badTestCreds.apiKey, siteID: targetSiteID })
         .expect(403, done);
     });
-    it('check config GET (/site/config) w/ good APIKey/SiteID', (done) => {
+    it('checking config GET (/site/config) w/ good APIKey/SiteID', (done) => {
       request(svr)
         .get('/site/config')
         .query({ apiKey: testCreds.apiKey, siteID: targetSiteID })
@@ -254,19 +257,19 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check config PUT (/site/config) w/ wrong security context', (done) => {
+    it('checking config PUT (/site/config) w/ wrong security context', (done) => {
       request(svr)
         .put('/site/config')
         .send({ apiKey: testCreds.apiKey, siteID: targetSiteID, title: 'New Site' })
         .expect(403, done);
     });
-    it('check config PUT (/site/config)', (done) => {
+    it('checking config PUT (/site/config)', (done) => {
       request(svr)
         .put('/site/config')
         .send({ apiKey: testCreds.apiKey, secret: testCreds.secret, siteID: targetSiteID, title: 'New Site' })
         .expect(200, done);
     });
-    it('check config GET (/site/config) updated info', (done) => {
+    it('checking config GET (/site/config) updated info', (done) => {
       request(svr)
         .get('/site/config')
         .query({ apiKey: testCreds.apiKey, siteID: targetSiteID })
@@ -285,55 +288,55 @@ describe('REST Endpoint Tests', () => {
   // =============================================================
   describe('Field Controller', () => {
     // TODO: Test - Add Field routes
-    it('check field POST (/site/field) w/ wrong security context', (done) => {
+    it('checking field POST (/site/field) w/ wrong security context', (done) => {
       request(svr)
         .post('/site/field')
         .send({ apiKey: testCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField' })
         .expect(403, done);
     });
-    it('check field POST (/site/field)', (done) => {
+    it('checking field POST (/site/field)', (done) => {
       request(svr)
         .post('/site/field')
         .send({ apiKey: testCreds.apiKey, secret: testCreds.secret, dealerID: testDealer, fieldLabel: 'TestField' })
         .expect(200, done);
     });
-    it('check field GET (/site/field/all) w/ mismatched APIKey/DealerID', (done) => {
+    it('checking field GET (/site/field/all) w/ mismatched APIKey/DealerID', (done) => {
       request(svr)
         .get('/site/field/all')
         .query({ apiKey: badTestCreds.apiKey, dealerID: testDealer })
         .expect(403, done);
     });
-    it('check field GET (/site/field/all) w/ good APIKey/DealerID', (done) => {
+    it('checking field GET (/site/field/all) w/ good APIKey/DealerID', (done) => {
       request(svr)
         .get('/site/field/all')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer })
         .expect(200, done);
     });
-    it('check field GET (/site/field) w/ mismatched APIKey/DealerID', (done) => {
+    it('checking field GET (/site/field) w/ mismatched APIKey/DealerID', (done) => {
       request(svr)
         .get('/site/field')
         .query({ apiKey: badTestCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField' })
         .expect(403, done);
     });
-    it('check field GET (/site/field) w/ good APIKey/DealerID', (done) => {
+    it('checking field GET (/site/field) w/ good APIKey/DealerID', (done) => {
       request(svr)
         .get('/site/field')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField' })
         .expect(200, done);
     });
-    it('check field PUT (/site/field) w/ wrong security context', (done) => {
+    it('checking field PUT (/site/field) w/ wrong security context', (done) => {
       request(svr)
         .put('/site/field')
         .send({ apiKey: testCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField', textContent: 'Test' })
         .expect(403, done);
     });
-    it('check field PUT (/site/field)', (done) => {
+    it('checking field PUT (/site/field)', (done) => {
       request(svr)
         .put('/site/field')
         .send({ apiKey: testCreds.apiKey, secret: testCreds.secret, dealerID: testDealer, fieldLabel: 'TestField', textContent: 'Test' })
         .expect(200, done);
     });
-    it('check field GET (/site/field) updated info', (done) => {
+    it('checking field GET (/site/field) updated info', (done) => {
       request(svr)
         .get('/site/field')
         .query({ apiKey: testCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField' })
@@ -345,13 +348,13 @@ describe('REST Endpoint Tests', () => {
           done(err);
         });
     });
-    it('check field DELETE (/site/config) w/ wrong security context', (done) => {
+    it('checking field DELETE (/site/config) w/ wrong security context', (done) => {
       request(svr)
         .delete('/site/field')
         .send({ apiKey: testCreds.apiKey, dealerID: testDealer, fieldLabel: 'TestField' })
         .expect(403, done);
     });
-    it('check field DELETE (/site/config)', (done) => {
+    it('checking field DELETE (/site/config)', (done) => {
       request(svr)
         .delete('/site/field')
         .send({ apiKey: testCreds.apiKey, secret: testCreds.secret, dealerID: testDealer, fieldLabel: 'TestField' })
@@ -363,14 +366,14 @@ describe('REST Endpoint Tests', () => {
   // Admin Post-Tests
   // =============================================================
   describe('Admin Controller Post-Tests', () => {
-    it('check revokeSitePermissions POST (/site/admin/revokeSitePermissions)', (done) => {
+    it('checking revokeSitePermissions POST (/site/admin/revokeSitePermissions)', (done) => {
       request(svr)
         .post('/site/admin/revokeSitePermissions')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, targetAPIKey: testCreds.apiKey, siteID: targetSiteID})
         .set('Content-Type', 'application/json')
         .expect(200, done);
     });
-    it('check deleteSite POST (/site/admin/deleteSite)', (done) => {
+    it('checking deleteSite POST (/site/admin/deleteSite)', (done) => {
       request(svr)
         .post('/site/admin/deleteSite')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, siteID: targetSiteID})
@@ -378,14 +381,14 @@ describe('REST Endpoint Tests', () => {
         .expect(200, done);
     });
     // Delete created API Key
-    it('check deleteAPIKey POST (/site/admin/deleteAPIKey)', (done) => {
+    it('checking deleteAPIKey POST (/site/admin/deleteAPIKey)', (done) => {
       request(svr)
         .post('/site/admin/createAPIKey')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret, targetAPIKey: testCreds.apiKey })
         .set('Content-Type', 'application/json')
         .expect(200, done);
     });
-    it('check flushCache POST (/site/admin/flushCache)', (done) => {
+    it('checking flushCache POST (/site/admin/flushCache)', (done) => {
       request(svr)
         .post('/site/admin/flushCache')
         .send({ apiKey: superTestCreds.apiKey, secret: superTestCreds.secret })
